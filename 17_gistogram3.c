285f9b00fe74f4e546ed94de8b55d0644e32bf86
Sergey Ierusalimov <wint@wint-osx2.local>
2017-05-04 23:16:11 +0300

    .

--- 17_gistogram3.c
+++ 17_gistogram3.c
@@ -1,47 +1,9 @@
 #include <stdio.h>
 
-#define IN 1
-#define OUT 0
-
 int main() {
-    int c, i, state, len;
-    int gist[10];
-    state = OUT;
-    len = 0;
-    for (i = 0; i < 10; i++)
-        gist[i] = 0;
-    while ((c = getchar()) != EOF) {
-        if (c == '\t' || c == ' ' || c == '\n') {
-            if (state == IN) {
-                if (len < 10)
-                    gist[len]++;
-                else 
-                    gist[0]++;
-            }
-            len = 0;
-            state = OUT;
-        }
-        else {
-            len++;
-            state = IN;
-        }
-    }
-    if (len > 0 && len < 10)
-        gist[len]++;
-    else if (len >= 10)
-        gist[0]++;
-    printf("m ");
-    while (gist[0] > 0) {
-        putchar('|');
-        gist[0]--;
-    }
-    putchar('\n');
-    for (i = 1; i < 10; i++) {
-        printf("%d ", i);
-        while (gist[i] > 0) {
-            putchar('|');
-            gist[i]--;
-        }
-        putchar('\n');
+    int c;
+    for (c = 0; c < 256; c++) {
+        putchar(c);
+        putchar(' ');
     }
 }
