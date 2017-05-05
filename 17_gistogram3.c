0df9bb8323187abdb7aab5da16f275b53f3d35fe
Wint <wint@wint.local>
2017-05-05 23:27:42 +0300

    final

--- 17_gistogram3.c
+++ 17_gistogram3.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-// dig - 48
-// upper - 65
-// lower  - 97
     int c, i;
     int digit[10], upper[26], lower[26];
     for (i = 0; i < 10; i++)
@@ -24,18 +21,37 @@ int main() {
 
     for (i = 0; i < 10; i++) {
         if (digit[i] > 0) {
-            putchar(i + '0: ');
+            putchar(i + '0');
+            printf(": ");
             while (digit[i] > 0) {
                 putchar('#');
+                digit[i]--;
             }
             putchar('\n');
-//            printf("%c: %d, ", ('0' + i), digit[i]);
         }
     }
-// help
-/*    for (c = 0; c < 256; c++) {
-        printf("%d-%c", c, c);
-        putchar(' ');
-    } */
-        putchar('\n');
+
+    for (i = 0; i < 26; i++) {
+        if (upper[i] > 0) {
+            putchar(i + 'A');
+            printf(": ");
+            while (upper[i] > 0) {
+                putchar('#');
+                upper[i]--;
+            }
+            putchar('\n');
+        }
+    }
+
+    for (i = 0; i < 26; i++) {
+        if (lower[i] > 0) {
+            putchar(i + 'a');
+            printf(": ");
+            while (lower[i] > 0) {
+                putchar('#');
+                lower[i]--;
+            }
+            putchar('\n');
+        }
+    }
 }
