7a7f63075256998970a3d159e77b749589fddeaf
Sergey Ierusalimov <wint@wint-osx2.local>
2017-05-05 23:09:19 +0300

    work

--- 17_gistogram3.c
+++ 17_gistogram3.c
@@ -8,10 +8,10 @@ int main() {
     int digit[10], upper[26], lower[26];
     for (i = 0; i < 10; i++)
         digit[i] = 0;
-    for (i = 0; i < 26; i++)
+    for (i = 0; i < 26; i++) {
         upper[i] = 0;
-    for (i = 0; i < 26; i++)
         lower[i] = 0;
+    }
 
     while ((c = getchar()) != EOF) {
         if (c >= '0' && c <= '9')
@@ -21,10 +21,21 @@ int main() {
         else if (c >= 'a' && c <= 'z')
             lower[c - 'a']++;
     }
+
+    for (i = 0; i < 10; i++) {
+        if (digit[i] > 0) {
+            putchar(i + '0: ');
+            while (digit[i] > 0) {
+                putchar('#');
+            }
+            putchar('\n');
+//            printf("%c: %d, ", ('0' + i), digit[i]);
+        }
+    }
 // help
-    for (c = 0; c < 256; c++) {
+/*    for (c = 0; c < 256; c++) {
         printf("%d-%c", c, c);
         putchar(' ');
-    }
+    } */
         putchar('\n');
 }
