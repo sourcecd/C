feebc75f65845152399b3ab1077fb0f39ceed9b6
Sergey Ierusalimov <wint@95.108.170.144-red.dhcp.yndx.net>
2017-05-11 11:41:22 +0300

    final

--- 20_chararrays.c
+++ 20_chararrays.c
@@ -3,7 +3,7 @@
 #define MAXLINE 1000
 
 /* Function prototypes */
-int getline(char line[], int MAXLINE);
+int getlines(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* Main function */
@@ -14,7 +14,7 @@ int main() {
     char longest[MAXLINE];
     max = 0;
 
-    while ((len = getline(line, MAXLINE)) > 0) {
+    while ((len = getlines(line, MAXLINE)) > 0) {
         if (len > max) {
             max = len;
             copy(longest, line);
@@ -26,9 +26,26 @@ int main() {
     return 0;
 }
 
-int getline(char s[], int lim) {
+int getlines(char s[], int lim) {
     int i;
     int c;
 
-    for (i = 0; i < (lim - 1) && )
+    for (i = 0; i < (lim - 1) && ((c = getchar()) != EOF) && c != '\n'; i++) {
+        s[i] = c;
+    }        
+    if (c == '\n') {
+        s[i] = c;
+        i++;
+    }
+    s[i] = '\0';
+    return i;
 }
+
+void copy(char to[], char from[]) {
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0') {
+        i++;
+    }
+}
+
