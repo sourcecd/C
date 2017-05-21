deef9f517467bc91410f51628dc085e373418245
Sergey Ierusalimov <wint@wint-osx2.local>
2017-05-21 17:11:54 +0300

    ok

--- 21_char_arrays_unlim.c
+++ 21_char_arrays_unlim.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #define MAXLINE 8
+#define OVERFLOW 1
+#define NORMAL 0
 
 /* Function prototypes */
 int getlines(char line[], int maxline);
@@ -10,18 +12,41 @@ void copy(char to[], char from[]);
 int main() {
     int len;
     int max;
+    int tmplen;
+    int overfl;
     char line[MAXLINE];
     char longest[MAXLINE];
+    char tmpline[MAXLINE];
     max = 0;
+    tmplen = 0;
+    overfl = NORMAL;
 
     while ((len = getlines(line, MAXLINE)) > 0) {
-        if (len > max) {
-            max = len;
-            copy(longest, line);
+        if (len < MAXLINE - 1 || line[MAXLINE - 2] == '\n') {
+            if (overfl == NORMAL) {
+                if (len > max) {
+                    max = len;
+                    copy(longest, line);
+                }
+            } else {
+                if (tmplen > max) {
+                    max = tmplen;
+                    copy(longest, tmpline);
+                }
+            }
+            overfl = NORMAL;
+        } else {
+            if (overfl == NORMAL) {
+                tmplen = 0;
+                copy(tmpline, line);
+            }
+            tmplen += len;
+            overfl = OVERFLOW;
         }
     }
     if (max > 0) {
-        printf("%s", longest);
+        printf("%s\n", longest);
+        printf("max line length: %d\n", max);
     }
     return 0;
 }
@@ -30,13 +55,9 @@ int getlines(char s[], int lim) {
     int i;
     int c;
 
-    for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
-        if (i < (lim - 2)) {
-            s[i] = c;
-        } else {
-            pass
-        }
-    }
+    for (i = 0; i < (lim - 1) && ((c = getchar()) != EOF) && c != '\n'; i++) {
+        s[i] = c;
+    }        
     if (c == '\n') {
         s[i] = c;
         i++;
